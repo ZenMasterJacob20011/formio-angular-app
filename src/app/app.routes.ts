3c17e72378cf4a99d6b010eba2ad2935da2252cb
ZenMasterJacob20011 <jacobnichols@form.io>
2025-02-23 20:00:53 -0600

    added bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {UserLoginComponent} from './user-login/user-login.component';
 
 export const routes: Routes = [
   {
-    path: 'auth/login',
+    path: '',
     component: UserLoginComponent
   }
 ];
