8ab3b987c62a8cfe942f93af7d95c62e0641ffdd
ZenMasterJacob20011 <jacobnichols@form.io>
2025-03-02 19:46:54 -0600

    added forms and resources panels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,26 @@ import {Component} from '@angular/core';
 import {UserLoginComponent} from './auth/user-login/user-login.component';
 import {FormioAuthService} from '@formio/angular/auth';
 import {InfoPanelComponent} from './info-panel/info-panel.component';
+import {FormioGrid} from '@formio/angular/grid';
 
 @Component({
   selector: 'app-root',
   imports: [
     UserLoginComponent,
     InfoPanelComponent,
+    FormioGrid,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'formio-angular-app';
+
   constructor(public service: FormioAuthService) {
 
+  }
+
+  isActionAllowed = (action: string) => {
+    return action !== 'formCreate';
   }
 }
