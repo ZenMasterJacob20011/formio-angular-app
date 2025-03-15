a2990152a063c0ec7292d776843795758a29351c
ZenMasterJacob20011 <jacobnichols@form.io>
2025-03-14 21:51:20 -0500

    created form page

---
+++ src/app/form-create/form-create.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {editType, FormEditComponent} from '../form-edit/form-edit.component';
+import {FormioServiceWrapper} from '../services/formio.service.wrapper';
+import {ActivatedRoute, RouterLink, RouterLinkActive} from '@angular/router';
+import {formType} from '../app.component';
+
+@Component({
+  selector: 'app-form-create',
+  imports: [
+    FormEditComponent
+  ],
+  templateUrl: './form-create.component.html',
+  styleUrls: ['./form-create.component.css', '../app.component.css']
+})
+export class FormCreateComponent {
+  formType: formType | undefined
+  constructor(protected formioServiceWrapper: FormioServiceWrapper, activatedRoute: ActivatedRoute) {
+    formioServiceWrapper.form = {};
+    activatedRoute.data.subscribe((data) => {
+      this.formType = data['formType'];
+    })
+  }
+
+  protected readonly editType = editType;
+}
