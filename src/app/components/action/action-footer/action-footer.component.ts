a2d8a24456f8d0bfd6868d07b77978739fdfa7d8
ZenMasterJacob20011 <jacobnichols@form.io>
2025-03-22 14:54:08 -0500

    created form actions page

---
+++ src/app/components/action/action-footer/action-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {GridFooterComponent} from '@formio/angular/grid';
+
+@Component({
+  selector: 'app-action-footer',
+  imports: [],
+  templateUrl: './action-footer.component.html',
+  styleUrl: './action-footer.component.css'
+})
+export class ActionFooterComponent extends GridFooterComponent{
+
+}
--- src/app/components/tabs/actions-tab/actions-tab.component.ts
+++ src/app/components/tabs/actions-tab/actions-tab.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import {FormioGrid} from '@formio/angular/grid';
+import {ActionHeaderComponent} from '../../action/action-header/action-header.component';
+import {ActionBodyComponent} from '../../action/action-body/action-body.component';
+import {ActionFooterComponent} from '../../action/action-footer/action-footer.component';
 
 @Component({
   selector: 'app-actions-tab',
-  imports: [],
+  imports: [
+    FormioGrid
+  ],
   templateUrl: './actions-tab.component.html',
   styleUrl: './actions-tab.component.css'
 })
 export class ActionsTabComponent {
+  components: any
 
+  constructor() {
+    this.components = {};
+    this.components.header = ActionHeaderComponent;
+    this.components.body = ActionBodyComponent;
+    this.components.footer = ActionFooterComponent;
+  }
 }
