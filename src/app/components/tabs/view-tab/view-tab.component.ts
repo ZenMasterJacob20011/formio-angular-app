c9790096b1ca59e16f73b566938eefeba29045fc
ZenMasterJacob20011 <jacobnichols@form.io>
2025-03-22 14:21:25 -0500

    create view data page

--- src/app/components/tabs/view-tab/view-tab.component.ts
+++ src/app/components/tabs/view-tab/view-tab.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {FormioGrid} from '@formio/angular/grid';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-view-tab',
-  imports: [],
+  imports: [
+    FormioGrid
+  ],
   templateUrl: './view-tab.component.html',
   styleUrl: './view-tab.component.css'
 })
 export class ViewTabComponent {
+  formUrl: string
 
+  constructor(private activatedRouter: ActivatedRoute) {
+    this.formUrl = `/form/${activatedRouter.parent?.snapshot.paramMap.get('id')}`;
+  }
 }
