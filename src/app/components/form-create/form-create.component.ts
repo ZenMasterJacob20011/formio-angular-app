05969e379d84ee64c50634be302fee20aa7cadab
ZenMasterJacob20011 <jacobnichols@form.io>
2025-03-22 22:18:51 -0500

    fixed new to edit bug

--- src/app/components/form-create/form-create.component.ts
+++ src/app/components/form-create/form-create.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {editType, FormEditComponent} from '../form-edit/form-edit.component';
 import {FormioServiceWrapper} from '../../services/formio.service.wrapper';
-import {ActivatedRoute, RouterLink, RouterLinkActive} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 import {formType} from '../../app.component';
 
 @Component({
@@ -12,13 +12,18 @@ import {formType} from '../../app.component';
   templateUrl: './form-create.component.html',
   styleUrls: ['./form-create.component.css', '../../app.component.css']
 })
-export class FormCreateComponent {
+export class FormCreateComponent implements OnDestroy {
   formType: formType | undefined
+
   constructor(protected formioServiceWrapper: FormioServiceWrapper, activatedRoute: ActivatedRoute) {
     formioServiceWrapper.form = {};
     activatedRoute.data.subscribe((data) => {
       this.formType = data['formType'];
     })
+  }
+
+  ngOnDestroy() {
+    delete this.formioServiceWrapper.form
   }
 
   protected readonly editType = editType;
