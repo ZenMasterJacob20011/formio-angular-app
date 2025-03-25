ada5646f09cf4af563c53025910379d36b4e1cc5
ZenMasterJacob20011 <jacobnichols@form.io>
2025-03-24 23:45:08 -0500

    populated actions select with options

--- src/app/components/action/action-footer/action-footer.component.ts
+++ src/app/components/action/action-footer/action-footer.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
 import {GridFooterComponent} from '@formio/angular/grid';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-action-footer',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './action-footer.component.html',
   styleUrl: './action-footer.component.css'
 })
 export class ActionFooterComponent extends GridFooterComponent{
+  actions: {value: string, label: string}[]
+  constructor() {
+    super();
+    this.actions = this.getActions();
+  }
 
+  getActions(){
+    return [
+      {
+        value: 'email',
+        label: 'Email'
+      },
+      {
+        value: 'login',
+        label: 'Login'
+      },
+      {
+        value: 'resetpassword',
+        label: 'Reset Password'
+      },
+      {
+        value: 'roleassignment',
+        label: 'Role Assignment'
+      },
+      {
+        value: 'savesubmission',
+        label: 'Save Submission'
+      },
+      {
+        value: 'webhook',
+        label: 'Webhook'
+      }
+    ]
+  }
 }
